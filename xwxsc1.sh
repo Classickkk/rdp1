b70e192e02d080e0c847f41bdfc3aabc3827d0f5
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-11-27 23:57:54 -0300

    Update xwxsc1.sh

--- xwxsc1.sh
+++ xwxsc1.sh
@@ -1,4 +1,4 @@
 wget -O goggle.deb www.dropbox.com/scl/fi/srsllbzeb2ili57kud8ox/google-chrome-stable_current_amd64.deb?rlkey=ezhrrllcb1fm69rpqb0v8agdt&raw=1
-dpkg -i goggle*
+dpkg -i goggle.deb
 echo 'Instalado com Sucesso!'
 cd BOT && sh ligar.sh
