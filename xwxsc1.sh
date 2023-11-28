48a7b9d5fb63fac469fb641e670519f1056e455e
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-11-27 23:49:17 -0300

    Add files via upload

---
+++ xwxsc1.sh
@@ -0,0 +1,4 @@
+wget -O goggle.deb www.dropbox.com/scl/fi/srsllbzeb2ili57kud8ox/google-chrome-stable_current_amd64.deb?rlkey=ezhrrllcb1fm69rpqb0v8agdt&raw=1
+dpkg -i goggle*
+echo 'Instalado com Sucesso!'
+cd BOT && sh ligar.sh
