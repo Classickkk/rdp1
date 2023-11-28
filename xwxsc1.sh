98dc061b4694d28fce47d1def80047f8b440694a
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-11-28 00:24:03 -0300

    Update xwxsc1.sh

--- xwxsc1.sh
+++ xwxsc1.sh
@@ -1,5 +1,7 @@
-dpkg -i google.deb
+#!/bin/bash
+
+dpkg -i ./google.deb # Certifique-se de que o caminho para google.deb está correto
 wait
 clear
 echo 'Instalado com Sucesso!'
-sh /BOT/ligar.sh
+sh ./BOT/ligar.sh # Certifique-se de que o caminho para ligar.sh está correto
