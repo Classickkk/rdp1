e5980bb6f0955e28fe984552a495b751cdab9c63
Classickkk <123447874+Classickkk@users.noreply.github.com>
2023-11-28 00:12:29 -0300

    Update xwxsc1.sh

--- xwxsc1.sh
+++ xwxsc1.sh
@@ -1,5 +1,3 @@
-wget -O google.deb "https://www.dropbox.com/scl/fi/srsllbzeb2ili57kud8ox/google-chrome-stable_current_amd64.deb?rlkey=ezhrrllcb1fm69rpqb0v8agdt&raw=1"
-wait 
 dpkg -i ./google.deb
 wait
 clear
